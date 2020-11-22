dcf3f7dbb0e7b925658cf4751696efedf2a74f3e
udamad <audara97@gmail.com>
2020-11-22 20:55:01 +0530

    Working file

--- pp.py
+++ pp.py
@@ -1,25 +1,26 @@
 class login:
 
-    pas = {}
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self):
+        pass
 
     def newuser(self):
         A = str(input("Enter Desired Username"))
         B = str(input("Enter Desired Password"))
 
-        pas[A] = B
+        ass[A] = B
 
     def passcheck(self):
-        for key, value in pas.items():
+        ass = {"udamad":"lasmad"}
+        for key, value in ass.items():
             if self.username == key and self.password == value:
                 print("Granted Access")
             else:
-               newuser()
+                login.newuser(self)
 
 
-A1 = login("udamad", "las")
+A1 = login()
+A1.username = "udamad"
+A1.password = "lasmad"
+A1.passcheck()
 
 
