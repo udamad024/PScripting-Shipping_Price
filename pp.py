a77b038437d5fc47e74174d9f3894779fe9dcaf8
udamad <audara97@gmail.com>
2020-11-24 14:10:27 +0530

    alright

--- pp.py
+++ pp.py
@@ -1,26 +1,25 @@
+def ground_shipping(weight):
+  if weight > 0 and weight < 2:
+    return weight * 1.50 + 20
+  elif weight > 2 and weight <= 6:
+    return weight * 3 + 20
+  elif weight > 6 and weight <= 10:
+    return weight * 4 + 20
+  elif weight > 10:
+    return weight * 4.75 + 20
 
-ass = {"udamad": "lasmad"}
+premium_ground_shipping = 125
 
-class login:
+def drone_shipping(weight):
+  if weight > 0 and weight < 2:
+    return weight * 4.50
+  elif weight > 2 and weight <= 6:
+    return weight * 9
+  elif weight > 6 and weight <= 10:
+    return weight * 12
+  elif weight > 10:
+    return weight * 14.25
 
-    def __init__(self):
-        pass
-
-
-    def passcheck(self):
-
-        for key, value in ass.items():
-            if self.username == key and self.password == value:
-                print("Granted Access")
-            else:
-                A = str(input("Enter Desired Name: "))
-                B = str(input("Enter Desired Password: "))
-                ass[A] = B
-
-
-A1 = login()
-A1.username = str(input("Enter Username: "))
-A1.password = str(input("Password: "))
-A1.passcheck()
+print(drone_shipping(1.5))
 
 
